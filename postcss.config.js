d82bc32c1e1549edc1fcd81b825b7af1c876c7b1
vishnupv <vishnupvstar1@gmail.com>
2026-08-17 18:21:01 +0530

    tailwind fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        'bounce-slow': 'bounce 3s infinite',
-      }
-    },
-  },
-  plugins: [],
+export default{
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+},
 }
